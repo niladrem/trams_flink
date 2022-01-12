22ca056af96f2fca43406e2cb4605690ea99b6a4
niladrem <aurbala@gmail.com>
2022-01-12 22:29:18 +0100

    format date

--- src/main/java/pw/mini/function/TramToStringFunction.java
+++ src/main/java/pw/mini/function/TramToStringFunction.java
@@ -4,7 +4,11 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.json.JSONObject;
 import pw.mini.Tram;
 
+import java.time.format.DateTimeFormatter;
+
 public class TramToStringFunction implements MapFunction<Tram, String> {
+
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @Override
     public String map(Tram value) throws Exception {
@@ -16,7 +20,7 @@ public class TramToStringFunction implements MapFunction<Tram, String> {
         obj.put("Lines", value.getLines());
         obj.put("Lon", value.getLon());
         obj.put("VehicleNumber", value.getVehicleNumber());
-        obj.put("Time", value.getTime());
+        obj.put("Time", value.getTime().format(formatter));
         obj.put("Lat", value.getLat());
         obj.put("Brigade", value.getBrigade());
 
